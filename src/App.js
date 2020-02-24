0ee792df47f42888679c9542d1c09ff6a5ad2adc
hana-ji <55686380+hana-ji@users.noreply.github.com>
2020-02-24 15:01:18 +0900

    state와 props

--- src/App.js
+++ src/App.js
@@ -6,34 +6,31 @@ import './App.css';
 
 
 class App extends Component {
-  // 클래스형 컴포넌트에서 프롭스 쓸 때 필요한 애 (state 값 초기화함)
-  // 컴포넌트가 실행될 때 constructor가 있으면 걔가 제일 먼저 실행되서 초기화를 담당함
   constructor(props){
     super(props);
-    // 스테이트값 초기화
+    // 부모인 App의 입장에서는 state라는 내부정보 사용
     this.state = {
-      // subject(property)의 값을 다시 객체로 줌 (state화 할것)
-      subject: {title:"WEB",sub:"World Wide Web!" }
+      subject: {title:"WEB", sub:"World Wide Web!" },
+      contents: [
+        {id:1, title:'HTML', desc:'HTML is information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ]
     }
   }
   render() {
     return(
       <div className="App">
-        {/* "사이에 쓰면 문자열", {사이에 쓰면 자바스크립트 코드} */}
         <Subject 
           title={this.state.subject.title} 
           sub={this.state.subject.sub} />
         <Subject title="hana" sub="지하나!" />
-        <TOC />
+        {/* 자식한테 전달할 때는 props로 사용*/}
+        <TOC data={this.state.contents} />
         <Content title="HTML" desc="HTML is HyperTextMarkup Language." />
       </div>
     );
   }
 }
-
-/* App이 내부적으로 사용할 상태 = state 형태
-   상위coponent(App)의 state값(this.state ={subject: {title:"WEB"}})을 
-   하위 컴포넌트(Subject)의 props의 값(title={this.state.subject.title})으로 전달하는것은 
-   얼마든지 가능하다 */
 
 export default App;
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -3,12 +3,23 @@ import React, { Component } from 'react';
 
 class TOC extends Component {
 render() {
+    // list에 나타날 애들을 list라는 배열에 담을것
+    var lists = [];
+    // TOC가 내부적으로 갖고있는 값
+    var data = this.props.data
+    // 반복문
+    var i = 0;
+    // data의 길이만큼 반복 한다
+    while(i < data.length){
+        // 반복문이 실행될때마다 푸쉬 해줄 거임
+        lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>);
+        // 반복 할때마다 i의 값은 i+1 이 된다.
+        i = i +1;
+    }
     return(
     <nav>
         <ul>
-            <li><a href="1.html">HTML</a></li>
-            <li><a href="2.html">CSS</a></li>
-            <li><a href="3.html">JavaScript</a></li>
+            {lists}
         </ul>
     </nav>
     );
