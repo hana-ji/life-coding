f6640972fe4a36438ba066fbde0d895c372e11fa
hana-ji <55686380+hana-ji@users.noreply.github.com>
2020-02-24 21:38:58 +0900

    bind()사용법 근데 () => 사용하면 바인드 안써도 됨 무비앱 코드 참고

--- src/App.js
+++ src/App.js
@@ -36,12 +36,15 @@ class App extends Component {
         <header>
             <h1><a href="/" onClick={function(e){
               e.preventDefault();
-              // setState를 사용해서 변한거를 리로드 시켜야함 this.state.mode = 'welcome';
+              // setState를 사용해서 변한거를 리로드 시켜야함 this.state.mode = 'welcome';는 안됨
               this.setState({
                 mode:'welcome'
               });
             //이벤트를 설치할 때 this를 찾을수 없어서 에러나면 함수가 끝난 직후에 
-            // bind(this)라고 적으면 됨
+            // .bind(this)라고 적으면 됨 (this의 대상 지정해주는 함수)
+            // (this)는 App이라는 component 자체를 가르키는 객체를 이 함수 안으로 주입해서 
+            // 함수 안에서 this가 객체가 되게하는 역할을 함
+            // render() 메서드 밖에서 화살표함수로 이벤트함수 만들어 넘겨주면 바인드 사용안해도 됨(니꼬 코드 참고)
             }.bind(this)}>{this.state.subject.title}</a></h1>
             {this.state.subject.sub}
         </header>
