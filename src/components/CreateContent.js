cf9044ac3516194b014097359043f7f55daf2e01
hana-ji <55686380+hana-ji@users.noreply.github.com>
2020-02-26 17:04:27 +0900

    create: form

--- src/components/CreateContent.js
+++ src/components/CreateContent.js
@@ -5,9 +5,17 @@ class CreateContent extends Component {
         return(
         <article>
             <h2>Create</h2>
-            <from>
-
-            </from>
+            <form action="/create_process" method="post"
+            // 서브밋 버튼을 클릭했을 때 서브밋 버튼을 포함하고 있는 폼 태그에 온서브밋이라는 이벤트를 설치하면 그 이벤트가 실행됨
+                onSubmit={function(e){
+                    e.preventDefault();
+                    alert("Submit!!!!");
+                }.bind(this)}
+            >
+                <p><input type="text" name="title" placeholder="title"></input></p>
+                <p><textarea name="desc"placeholder="description"></textarea></p>
+                <p><input type="submit"></input></p>
+            </form>
         </article>
         );
     }
