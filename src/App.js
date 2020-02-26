5749ae0ded92aec72b0d8ba62edaab98041b103c
hana-ji <55686380+hana-ji@users.noreply.github.com>
2020-02-26 13:17:24 +0900

    속성을 이용한 이벤트

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ class App extends Component {
     super(props);
     this.state = {
       mode: 'read',
+      // 기본으로 2번 컨텐트가 선택되게 함
+      selected_content_id:2,
       subject: {title:"WEB", sub:"World Wide Web!" },
       welcome:{title:'Welcome', desc:'Hello, React'},
       contents: [
@@ -25,8 +27,18 @@ class App extends Component {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
     } else if(this.state.mode === 'read'){
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+      var i = 0;
+      while(i < this.state.contents.length){
+        var data = this.state.contents[i]
+        if(data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc;
+          // 강제로 와일 문 끝냄/ 사용 시 조건문 끝나고 조건문 바깥 쪽이 실행됨
+          break;
+        }
+        i = i + 1;
+      }
+
     }
     return(
       <div className="App">
@@ -40,8 +52,14 @@ class App extends Component {
           }
           />
         <TOC 
-          onChangePage={function(){
-            this.setState({mode:'read'})
+          // 체인지페이지 이벤트가 발생했을 때
+          onChangePage={function(id){
+            // this.setState를 이용해서 mode의 값과 함께 selected id의 값 0 이라고 되있는 부분에 해주면 됨
+            this.setState({
+              mode:'read',
+              // 숫자열로 강제로 만들기 Number()
+              selected_content_id:Number(id)
+          });
           }.bind(this)}
          data={this.state.contents} />
         <Content title={_title} desc={_desc} />
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -11,13 +11,17 @@ render() {
         <li key={data[i].id}>
           <a 
             href={"/content/"+data[i].id}
+            data-id={data[i].id}
+            // 이벤트를 실행 시킬때 e의
             onClick={function(e){
+              // event 태그는 target 을 속성으로 가짐. 타겟은 이벤트가 발생한 태그 가르킴
               e.preventDefault();
-              this.props.onChangePage();
+              // 이 함수를 호출하는 코드에 인자로 넣어주기
+              // e의 target의 dataset의 id를 추출
+              this.props.onChangePage(e.target.dataset.id);
             }.bind(this)}
           >{data[i].title}</a>
         </li>);
-        // 반복 할때마다 i의 값은 i+1 이 된다.
         i = i +1;
     }
     return(
