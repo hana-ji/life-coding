bf8974f9363e50895def1c53a14b646aee3f76ee
hana-ji <55686380+hana-ji@users.noreply.github.com>
2020-02-27 15:22:08 +0900

    update구현

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import TOC from "./components/TOC";
 import ReadContent from "./components/ReadContent";
 import CreateContent from './components/CreateContent';
+import UpdateContent from './components/UpdateContent';
 import Subject from "./components/Subject";
 import Control from "./components/Control";
 import './App.css';
@@ -10,8 +11,6 @@ import './App.css';
 class App extends Component {
   constructor(props){
     super(props);
-    // = 3? 마지막 contents의 마지막 아이디와 같아야 함
-    // state값으로 하지않고 객체값으로 한 이유 : 데이터추가(푸쉬) 할때 id값을 뭐로할까? 할때 사용하는 정보일 뿐이라서
     this.max_content_id = 3;
     this.state = {
       mode: 'create',
@@ -25,44 +24,72 @@ class App extends Component {
       ]
     }
   }
-  render() {
+  getReadContent(){
+    var i = 0;
+      while(i < this.state.contents.length){
+        var data = this.state.contents[i]
+        if(data.id === this.state.selected_content_id) {
+          return data;
+          break;
+        }
+        i = i + 1;
+      }
+  }
+  // 함수로 만들면서 랜더는 슬림해짐
+  getContent(){
     var _title, _desc, _article = null;
     if(this.state.mode === 'welcome'){
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
       _article = <ReadContent title={_title} desc={_desc} />
     } else if(this.state.mode === 'read'){
-      var i = 0;
-      while(i < this.state.contents.length){
-        var data = this.state.contents[i]
-        if(data.id === this.state.selected_content_id) {
-          _title = data.title;
-          _desc = data.desc;
-          break;
-        }
-        i = i + 1;
-      }
-      _article = <ReadContent title={_title} desc={_desc} />
+      // 깔끔하게 리펙토링함
+      var _content = this.getReadContent();
+      _article = <ReadContent title={_content.title} desc={_content.desc} />
     } else if(this.state.mode === 'create'){
-      // submit 버튼이 클릭됬을 때 CreateContent의 이벤트로 설치된 함수를 실행
       _article = <CreateContent onSubmit={function(_title,_desc){
-        // add content to this.state.contents
-        // 위에 부분이 실행될 때 마다 기존에 아이디 값 + 1 증가
         this.max_content_id = this.max_content_id + 1;
-        // this.state.contents.push(
+        // var _contents = this.state.contents.concat(
         //   {id:this.max_content_id, title:_title, desc:_desc}
-        //   ); < 오리지널 데이터를 바꿈 나중에 개선할때 까다로움
-        // _contents = concat의 리턴값 (데이터 추가된 값)
-        var _contents = this.state.contents.concat(
-          {id:this.max_content_id, title:_title, desc:_desc}
-        );
-        // 기존에 가지고있던 값이 새롭게 만들어진 데이터로 교체됨
+        // ); 이 방법과 복제해서 하는 방법으로 배열 추가할수있음
+        var _contents = Array.from(this.state.contents);
+        // 복제했으니까 푸쉬편하게 사용해도 상관없음
+        _contents.push({id:this.max_content_id, title:_title, desc:_desc});
         this.setState({
-          contents:_contents
+          contents:_contents,
+          // 모드바꾸고 selected_content_id를 지금 추가한 것으로 바꿔주면 상세보기로 바로 보여줌
+          mode:'read',
+          selected_content_id: this.max_content_id
         });
-        // console.log(_title, _desc) 로 title과 desc 값 얻은거 확인
+      }.bind(this)}/>
+    } else if(this.state.mode === 'update'){
+      _content = this.getReadContent();
+      // _content라고 하는 값을 data로 주입
+      _article = <UpdateContent data={_content} onSubmit={
+        function(_id, _title, _desc){
+          // this.state.contents를 복사한 새로운 배열이 만들어짐(성능 튜닝할때 사용 (불변함))
+          var _contents = Array.from(this.state.contents);
+          // _contents안에있는 원소들을 뒤져서 id값이 우리가 수정하고자하는 것과 같은 원소 찾기
+          var i = 0;
+          while(i < _contents.length){
+            if(_contents[i].id === _id) {
+              _contents[i] = {id:_id, title:_title, desc:_desc};
+              break;
+            }
+            i = i + 1;
+          }
+          // 수정 후 수정한 내용으로 상세보기 까지 = mode만 바꾸면 됨
+          // 업데이트가 끝난 후
+          this.setState({
+            contents:_contents,
+            // 모드만 바꿔주면 상세보기도 가능
+            mode:'read'
+          });
       }.bind(this)}/>
     }
+    return _article;
+  }
+  render() {
     return(
       <div className="App">
         <Subject 
@@ -87,7 +114,7 @@ class App extends Component {
             mode:_mode
           });
         }.bind(this)} />
-        {_article}
+        {this.getContent()}
       </div>
     );
   }
--- src/components/CreateContent.js
+++ src/components/CreateContent.js
@@ -6,11 +6,8 @@ class CreateContent extends Component {
         <article>
             <h2>Create</h2>
             <form action="/create_process" method="post"
-            // 온서브밋 이벤트가 발생햇을 때 실행하도록 함
                 onSubmit={function(e){
                     e.preventDefault();
-                    // debugger; 로 경로 찾아서 해도되는데 찾기가 어려움 ㅜ
-                    // CreateConente onSubmit이라는 프롭스 호출
                     this.props.onSubmit(
                         e.target.title.value,
                         e.target.desc.value
